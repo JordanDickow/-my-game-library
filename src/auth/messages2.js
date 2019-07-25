1209689c2461487bd0733b83c5c199097671e760
jordandickow <jdickow96@gmail.com>
2019-07-25 16:09:03 -0400

    switched from OpenRead to Protected

---
+++ src/auth/messages2.js
@@ -0,0 +1,7 @@
+export default {
+  createGameSuccess: 'Here\'s your new game!',
+  createGameFailure: 'Well this is weird....',
+  updateGameSuccess: 'You updated the game!',
+  updateGameFailure: 'Check all the fields.',
+  deleteGameSuccess: 'No more game!'
+}
